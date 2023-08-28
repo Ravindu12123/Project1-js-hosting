4c6d383ec8d6594d8448f0a29523175bb5355abf
Ravindu12123 <140928846+Ravindu12123@users.noreply.github.com>
2023-08-28 15:53:27 +0530

    Add files via upload

---
+++ ins.js
@@ -0,0 +1,21 @@
+XMLHttpRequest.prototype.realSend = XMLHttpRequest.prototype.send;
+XMLHttpRequest.prototype.realOpen = XMLHttpRequest.prototype.open;
+console.prototype.realLog=console.prototype.log;
+
+const wind=window.open("","_blank");
+
+XMLHttpRequest.prototype.open = function (a,b) {
+    console.log(a,b);
+    wind.document.write(a+"\n"+b);
+    this.realOpen(a,b)
+}
+
+
+XMLHttpRequest.prototype.send = function(value) {
+  this.addEventListener("progress",function(){
+        console.log(value);
+        wind.document.write(value);
+    }, false);
+  //this.realSend.apply(this, arguments);
+  this.realSend(value);
+};
